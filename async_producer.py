3609be45bc731ca102ba18ebd7682c36ece8952d
Patrik Kormosi <patres.kormi@gmail.com>
2025-03-10 20:51:32 +0100

    create async_producer

---
+++ async_producer.py
@@ -0,0 +1,78 @@
+import time
+import heapq
+from collections import deque
+
+
+class Scheduler:
+    def __init__(self):
+        self.ready = deque()  # Functions ready to execute
+        self.sleeping = []
+        # A tie-braking mechanism to resolve cases where two co-routines would get
+        # scheduled with the same deadline (we would get a TypeError, because we cannot
+        # compare two functions).
+        self.sequence = 0
+
+    def call_soon(self, func):  # Also could be called `def schedule"
+        self.ready.append(func)
+
+    # My somewhat functioning implementation
+    def call_later(self, delay, func):
+        self.sequence += 1
+        deadline = time.time() + delay
+        heapq.heappush(self.sleeping, (deadline, self.sequence, func))
+
+    def run(self):
+        while self.ready or self.sleeping:
+            if not self.ready:
+                # Find the nearest deadline
+                deadline, _, func = heapq.heappop(self.sleeping)
+                delta = deadline - time.time()
+                if delta > 0:
+                    time.sleep(delta)
+                self.ready.append(func)
+            while self.ready:
+                func = self.ready.popleft()
+                func()
+
+
+scheduler = Scheduler()
+
+class AsyncQueue:
+    def __init__(self):
+        self.items = deque()
+        self.waiting = deque()  # All getters waiting for data
+
+    def put(self, item):
+        self.items.append(item)
+        if self.waiting:
+            func = self.waiting.popleft()
+            scheduler.call_soon(func)
+
+    def get(self, callback):
+        # Wait until an item is available, then return it
+        if self.items:
+            callback(self.items.popleft())
+        else:
+            self.waiting.append(lambda: self.get(callback))
+
+def producer(q, count):
+    for n in range(count):
+        print("Producing", n)
+        q.put(n)
+        time.sleep(1)
+    print("Producer done")
+    q.put(None)  # "Sentinel" to shut down
+
+
+def consumer(q):
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        print("Consuming", item)
+    print("Consumer done")
+
+
+q = queue.Queue()  # Thread-safe queue
+threading.Thread(target=producer, args=(q, 10)).start()
+threading.Thread(target=consumer, args=(q,)).start()
